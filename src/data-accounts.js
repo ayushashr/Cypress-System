08746a6ca6bb47f0b709f8f7c507650abf33595e
Ashley Liu <ashleyl1u095@gmail.com>
2025-04-08 13:27:29 -0400

    updated login page

---
+++ src/data-accounts.js
@@ -0,0 +1,31 @@
+const citizenAccounts = [
+  {
+    firstName: 'Ashley',
+    lastName: 'Liu',
+    email: 'ashleyliu@gmail.com',
+    password: '123456'
+  },
+  {
+    firstName: 'Citizen',
+    lastName: 'Citizen LastName',
+    email: 'citizen@cypress.com',
+    password: 'C123'
+  }
+]
+
+const adminAccounts = [
+  {
+    firstName: 'Ashley',
+    lastName: 'Liu',
+    email: 'ashleyliu@gmail.com',
+    password: '123456'
+  },
+  {
+    firstName: 'Admin',
+    lastName: 'Admin',
+    email: 'admin@cypress.com',
+    password: 'C123'
+  }
+]
+
+export {citizenAccounts, adminAccounts}
